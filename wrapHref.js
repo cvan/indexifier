d93abb5516b54f2d395f65ba64f0070303dcb11a
Christopher Van <cvan@mozilla.com>
2016-11-29 00:26:13 -0800

    add file sizes and bold-set directories

--- wrapHref.js
+++ wrapHref.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const prettyBytes = require('pretty-bytes');
 
 module.exports = function wrapHref(node, cwd) {
     const relativePath = path.relative(cwd, node.path);
-    return `<a href="./${relativePath}">${node.name}</a><br/>`;
+    if ('children' in node) {
+      return `<a href="./${relativePath}"><b>${node.name}</b></a> <br>`;
+    } else {
+      const size = prettyBytes(node.size);
+      return `<a href="./${relativePath}">${node.name}</a> (${size}) <br>`;
+    }
 };
